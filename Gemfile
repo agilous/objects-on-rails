6372d4e522f085ad0ec2727bb47958e324015b58
Bill Barnett <bill@agilo.us>
2011-11-19 01:39:10 -0500

    pristine Rails 3.1.2 app

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.2'
+gem 'haml'
+gem 'haml-rails', :group => 'development'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5.rc.2'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'cucumber'
+  gem 'cucumber-rails'
+end
+
+group :test do
+  gem 'turn', '0.8.2', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ObjectsOnRails::Application.config.secret_token = 'c1ac7a2100a4e711719b96c0d9842f11fa20c12d11b8728cbc01d8757777f96c96bc26bfccdcb60773e19019665e85bc0dc49555a2ee8fec28531081cd262358'
